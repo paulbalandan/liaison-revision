8a4b5b466beb40728f4ae90ec30faa737e539f17
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2020-08-07 01:25:42 +0800

    Add initial language files

---
+++ src/Language/en/Revision.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of Liaison Revision.
+ *
+ * (c) John Paul E. Balandan, CPA <paulbalandan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    // WriteGitignoreEntryCommand
+    'writablePathNotWritable'       => 'WRITEPATH is not writable by {0}.',
+    'gitignoreWriteDenied'          => '{0} is not allowed to write entries to `.gitignore`.',
+    'gitignoreFileMissing'          => '`.gitignore` is missing.',
+    'createGitignoreFile'           => 'Create a new .gitgnore file?',
+    'createGitignoreEntryFail'      => 'Failed creating entry in .gitignore.',
+    'createGitignoreEntryDuplicate' => 'There was already an entry in .gitignore.',
+    'createGitignoreEntrySuccess'   => 'Successfully created an entry to .gitignore.',
+];
