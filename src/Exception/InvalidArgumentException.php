c7d69e0812f76805ba6774c07e0278cb2fe2b746
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2020-08-06 22:58:05 +0800

    Library's exception classes

---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of Liaison Revision.
+ *
+ * (c) John Paul E. Balandan, CPA <paulbalandan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Liaison\Revision\Exception;
+
+/**
+ * InvalidArgumentException
+ *
+ * @package Liaison\Revision
+ */
+class InvalidArgumentException extends \InvalidArgumentException
+{
+}
---
+++ src/Exception/RevisionException.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of Liaison Revision.
+ *
+ * (c) John Paul E. Balandan, CPA <paulbalandan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Liaison\Revision\Exception;
+
+/**
+ * RevisionException
+ *
+ * Runtime update exceptions.
+ *
+ * @package Liaison\Revision
+ */
+class RevisionException extends \RuntimeException
+{
+}
