6c8d053f2289d62f36cfb5bbd3ee12d408c2e3d9
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2020-08-07 11:40:10 +0800

    Add LogicException

---
+++ src/Exception/LogicException.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is part of Liaison Revision.
+ *
+ * (c) John Paul E. Balandan, CPA <paulbalandan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Liaison\Revision\Exception;
+
+/**
+ * LogicException
+ */
+class LogicException extends \LogicException
+{
+}
